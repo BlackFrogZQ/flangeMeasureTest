ef6886e9de34f4f8c2ba70e067875b1893d92bcf
BlackFrogZQ <1275448553@qq.com>
2025-05-06 11:49:31 +0800

    加入相机参数部分

--- src/hal/HIK3DMVS/HIK3DMVS.h
+++ src/hal/HIK3DMVS/HIK3DMVS.h
@@ -2,48 +2,51 @@
 #include "Mv3dLpApi.h"
 #include "Mv3dLpDefine.h"
 #include "Mv3dLpImgProc.h"
+#include "HIK3DMVSDef.h"
 #include <windows.h>
 #include <iostream>
 #include <QtGui>
+#include <QObject>
 
-class CHIK3DMVS
+namespace TIGER_HIKROBOT3DCamera
 {
-public:
-    CHIK3DMVS(WId p_hWndDisplay);
-    ~CHIK3DMVS();
+    class CHIK3DMVS : public QObject
+    {
+    public:
+        CHIK3DMVS(WId p_hWndDisplay);
+        ~CHIK3DMVS();
 
-    void enumDevices();
-    void openCamera();
-    void closeCamera();
+        void enumDevices();
+        void openCamera();
+        void closeCamera();
 
-    static DWORD WINAPI staticProcessThread(LPVOID lpParam);
-    void processThread();
-    void startGrabImage();
-    void stopGrabImage();
+        void startGrabImage();
+        void stopGrabImage();
+        static DWORD WINAPI staticProcessThread(LPVOID lpParam);
+        void processThread();
+        void displayImage(MV3D_LP_IMAGE_DATA* p_imageData);
 
-    void displayImage(MV3D_LP_IMAGE_DATA* p_imageData);
-    int saveImage(Mv3dLpFileType p_enFileType);
-    void saveImageTiff();
-    void saveImageBMP();
-    void saveImageJPG();
-    void saveImageRAW();
+        void saveImage(QString p_filename);
 
-protected:
-    void init();
+    protected:
+        void init();
 
-private:
-    unsigned int m_nDevNum;
-    MV3D_LP_DEVICE_INFO m_stDeviceInfoList[20];
+    private:
+        unsigned int m_nDevNum;
+        MV3D_LP_DEVICE_INFO m_stDeviceInfoList[20];
 
-    bool m_bConnect;
-    void* m_handle;
+        bool m_bConnect;
+        void* m_handle;
 
-    bool m_bStartJob;
-    HANDLE m_hProcessThread;
-    DWORD m_threadid;
+        bool m_bStartJob;
+        HANDLE m_hProcessThread;
+        DWORD m_threadid;
 
-    QMutex m_mutex;
-    WId  m_hWndDisplay;
-    MV3D_LP_IMAGE_DATA m_stImageInfo;
-    unsigned char* m_pcDataBuf;
+        QMutex m_mutex;
+        WId  m_hWndDisplay;
+        MV3D_LP_IMAGE_DATA m_stImageInfo;
+        unsigned char* m_pcDataBuf;
+
+        int pErrorCode;
+    };
 };
---
+++ src/hal/HIK3DMVS/HIK3DMVSDef.cpp
@@ -0,0 +1,10 @@
+﻿#include "HIK3DMVSDef.h"
+
+namespace TIGER_HIKROBOT3DCamera
+{
+    CHIK3DCameraSetParas *HIKCameraSetPara()
+    {
+        static CHIK3DCameraSetParas g_HIK3DCameraSetPara;
+        return &g_HIK3DCameraSetPara;
+    }
+}
